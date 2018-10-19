d05ec9c0950bcd18c47ddc76e217991f1050152d
Your Name <you@example.com>
2018-10-19 16:07:14 +0100

    ..

--- OptimizacionII/TareaIII/LongStepPathFollowing.py
+++ OptimizacionII/TareaIII/LongStepPathFollowing.py
@@ -5,30 +5,55 @@ from scipy.sparse import csc_matrix
 from scipy.sparse.linalg import spsolve
 from scipy.sparse import identity
 from scipy.sparse import diags, hstack, vstack
-n=2 #nmero de variables
-m=2 #nmero de restricciones
+from scipy.optimize import linprog
 
-A = csc_matrix([[1, 1], [2, 0.5]], dtype=float)
-C = csc_matrix([-3, -2], dtype=float)
-b = np.array([5,8])
 
-s = np.array([5,8])
-x = np.array([5,8])
-llambda = np.array([9,8])
-#b = csc_matrix([[5], [8]], dtype=float)
 
-I = identity(n, dtype=float)
-S = diags(s)
-X = diags(x)
 
-rs = A.dot(llambda) + s -  C.data
-rb = A.dot(x) - b
-XSe = np.multiply(s, x)
-#XSe = 
-K1 = hstack([csc_matrix( (n,n)) , csc_matrix.transpose(A) ,I])
-K2 = hstack([A, csc_matrix( (m,m))  , csc_matrix( (m,n)) ])
-K3 = hstack([S, csc_matrix( (n,m)), X])
-K = vstack([K1,K2,K3])
-bb = np.hstack((rs, rb, XSe))
-x = scipy.sparse.linalg.spsolve(K, bb)
-print(x)
+
+def LongStepPath(A, c, b, s, x, llambda):
+   n=3 #nmero de variables
+   m=3 #nmero de restricciones
+   sigma = np.ones(n)*0.01
+   I = identity(n, dtype=float)
+   alpha = 0.001
+   for i in range(0,100):
+     S = diags(s)
+     X = diags(x) 
+     rs = A.dot(llambda) + s - c# C.data
+     rb = A.dot(x) - b
+     Mu = np.ones(n)*(np.inner(s, x)/n)
+     XSe = np.multiply(s, x) #- np.multiply(sigma,Mu)
+     K1 = hstack([csc_matrix( (n,n)) , csc_matrix.transpose(A) ,I])
+     K2 = hstack([A, csc_matrix( (m,m))  , csc_matrix( (m,n)) ])
+     K3 = hstack([S, csc_matrix( (n,m)), X])
+     K = vstack([K1, K2, K3])
+     bb = np.hstack((rs, rb, XSe))
+   #  print bb
+     xx = scipy.sparse.linalg.spsolve(K, -bb)
+     x = x + alpha*xx[0:n]
+     s = s + alpha*xx[n:2*n]
+     llambda = llambda + 0.01*xx[2*n: 2*n+m+1]
+   return x
+
+
+#A = csc_matrix([[1, 1], [2, 0.5]], dtype=float)
+A = csc_matrix( [ [1, 4, 8], [40,30,20], [3,2,4]] , dtype=float)
+
+
+#C = csc_matrix([-3, -2], dtype=float)
+c = np.array([70, 80, 85])
+b = np.array([4500, 36000,2700])
+
+
+s = np.array([1.0,1.0, 1.0])
+x = np.array([1, 1.429, 1])
+llambda = np.array([0.0,0.0, 0.0])
+##b = csc_matrix([[5], [8]], dtype=float)
+#
+print LongStepPath(A, c, b, s, x, llambda)
+#
+#print(x)
+
+res = linprog(c, A_ub = A.toarray(),b_ub= b, bounds=(0, None))
+print(res)
